kcl-lang/kcl-openapi

commit ad8f16834cc826cff4db4ed753e907b5d192195f

feat: protocol and mixin suffix (#146)
* feat: protocol and mixin suffix

Signed-off-by: Peefy <xpf6677@163.com>

* chore: update CI

Signed-off-by: Peefy <xpf6677@163.com>

---------

Signed-off-by: Peefy <xpf6677@163.com>

pkg/swagger/generator/model.go
@@ -1463,6 +1463,10 @@ func kclName(sch *spec.Schema, orig string) string {
 	if name != "" {
 		return name
 	}
+	// Add "Schema" suffix when name ends with "Protocol" or "Mixin"
+	if strings.HasSuffix(orig, "Protocol") || strings.HasSuffix(orig, "Mixin") {
+		return orig + "Schema"
+	}
 	return orig
 }
 

.github/workflows/main_darwin.yml
@@ -8,7 +8,7 @@ on:
      - main
 jobs:
   build-and-test:
-    runs-on: macos-12
+    runs-on: macos-15
     steps:
       - name: Git checkout
         uses: actions/checkout@v6

.github/workflows/main_linux.yml
@@ -15,7 +15,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        go-version: [ '1.20', '1.21', '1.22', '1.23' ]
+        go-version: [ '1.23', '1.24' ]
     steps:
       - name: Git checkout
         uses: actions/checkout@v6

commit 316cf27c8ade1178d2886ee5a660217d43dab8d6

fix: mixin/protocol schema suffix
fix feature and wrong behavior introduced by #146

* fix Mixin/Protocol schema suffix protection applying to property field names instead of generated schema type names: remove suffix logic from kclName (which is shared between property and schema naming) and add a protectedSchemaName helper applied only at inline schema creation in buildProperties

* add integration test case suffix_protected_mixin_protocol covering CRD properties whose names end with Mixin or Protocol

* fix CompareDir early-returning after first subdirectory, causing remaining files to be skipped in integration test comparison

Signed-off-by: Richard Hillmann <richard@flowers-software.de>

.gitignore
@@ -14,6 +14,10 @@ _build
 
 # tmp test data
 test_data/tmp*
+testdata/tmp*
+tmp_crd_*
+
+coverage.out
 
 vendor/
 

pkg/swagger/generator/model.go
@@ -247,7 +247,7 @@ func (schema *GenSchema) getBuiltInImports() map[string]importStmt {
 
 // getImportAsName infers the <import as> name by the context of all the existing import paths and the current pkg to be imported.
 // the parent package name will be added as prefix to avoid import conflict
-func getImportAsName(imp map[string]importStmt, pkg string, module string) string {
+func getImportAsName(imp map[string]importStmt, pkg, module string) string {
 	parts := strings.Split(pkg, ".")
 	asName := ""
 	for i := len(parts) - 1; i >= 0; i-- {
@@ -649,7 +649,7 @@ func (sg *schemaGenContext) buildProperties() error {
 
 		if tpe.IsComplexObject && tpe.IsAnonymous && len(v.Properties) > 0 {
 			// this is an anonymous complex construct: build a new type for it
-			pg := sg.makeNewSchema(sg.Name+swag.ToGoName(k), v)
+			pg := sg.makeNewSchema(protectedSchemaName(sg.Name+swag.ToGoName(k)), v)
 			pg.IsTuple = sg.IsTuple
 			if sg.Path == "" {
 				pg.Path = k
@@ -710,7 +710,7 @@ func (sg *schemaGenContext) buildProperties() error {
 			}
 
 			// set property name
-			var nm = filepath.Base(emprop.Schema.Ref.GetURL().Fragment)
+			nm := filepath.Base(emprop.Schema.Ref.GetURL().Fragment)
 			tr := sg.TypeResolver.NewWithModelName(kclName(&emprop.Schema, swag.ToGoName(nm)))
 			_, err := tr.ResolveSchema(sch, false, true)
 			if err != nil {
@@ -840,7 +840,7 @@ func newMapStack(context *schemaGenContext) (first, last *mapStack, err error) {
 		}
 
 		if !tpe.IsMap {
-			//reached the end of the rabbit hole
+			// reached the end of the rabbit hole
 			if tpe.IsComplexObject && tpe.IsAnonymous {
 				// found an anonymous object: create the struct from a newly created definition
 				nw := l.Context.makeNewSchema(l.Context.Name+" Anon", *l.Type.AdditionalProperties.Schema)
@@ -863,7 +863,7 @@ func newMapStack(context *schemaGenContext) (first, last *mapStack, err error) {
 		l = l.Next
 	}
 
-	//return top and bottom entries of this stack of AdditionalProperties
+	// return top and bottom entries of this stack of AdditionalProperties
 	return ms, l, nil
 }
 
@@ -1293,9 +1293,8 @@ func (sg *schemaGenContext) buildItems() error {
 }
 
 func (sg *schemaGenContext) buildAdditionalItems() error {
-	wantsAdditionalItems :=
-		sg.Schema.AdditionalItems != nil &&
-			(sg.Schema.AdditionalItems.Allows || sg.Schema.AdditionalItems.Schema != nil)
+	wantsAdditionalItems := sg.Schema.AdditionalItems != nil &&
+		(sg.Schema.AdditionalItems.Allows || sg.Schema.AdditionalItems.Schema != nil)
 
 	sg.GenSchema.HasAdditionalItems = wantsAdditionalItems
 	if wantsAdditionalItems {
@@ -1463,11 +1462,17 @@ func kclName(sch *spec.Schema, orig string) string {
 	if name != "" {
 		return name
 	}
-	// Add "Schema" suffix when name ends with "Protocol" or "Mixin"
-	if strings.HasSuffix(orig, "Protocol") || strings.HasSuffix(orig, "Mixin") {
-		return orig + "Schema"
-	}
 	return orig
+}
+
+// protectedSchemaName adds a "Schema" suffix to schema names that end with
+// KCL built-in keywords "Protocol" or "Mixin", to avoid conflicts with KCL's
+// special schema modifiers.
+func protectedSchemaName(name string) string {
+	if strings.HasSuffix(name, "Protocol") || strings.HasSuffix(name, "Mixin") {
+		return name + "Schema"
+	}
+	return name
 }
 
 func (sg *schemaGenContext) makeGenSchema() error {

pkg/utils/integrate_gen.go
@@ -165,7 +165,7 @@ func shouldIgnore(entry os.DirEntry) bool {
 	return !entry.IsDir() || strings.HasPrefix(entry.Name(), ".") || strings.HasPrefix(entry.Name(), "_") || strings.HasPrefix(entry.Name(), "fix_me_")
 }
 
-func CompareDir(a string, b string) error {
+func CompareDir(a, b string) error {
 	dirA, err := os.ReadDir(a)
 	if err != nil {
 		return fmt.Errorf("read dir %s failed when comparing with %s", a, b)
@@ -189,7 +189,10 @@ func CompareDir(a string, b string) error {
 			return fmt.Errorf("open file failed when compare, file path: %s", bPath)
 		}
 		if fA.IsDir() {
-			return CompareDir(aPath, bPath)
+			if err := CompareDir(aPath, bPath); err != nil {
+				return err
+			}
+			continue
 		}
 		linesA, err := readLines(aPath)
 		if err != nil {

pkg/kube_resource/generator/testdata/suffix_protected_mixin_protocol/crd.golden.yaml
@@ -1,0 +1,51 @@
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  annotations:
+    controller-gen.kubebuilder.io/version: v0.19.0
+  name: builtin_names.kcl-lang.io
+spec:
+  group: kcl-lang.io
+  names:
+    kind: BuiltinNames
+    listKind: BuiltinNamesList
+    plural: builtin_names
+    singular: builtin_name
+  scope: Namespaced
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        properties:
+          apiVersion:
+            description: ""
+            type: string
+          kind:
+            description: ""
+            type: string
+          metadata:
+            type: object
+          spec:
+            description: ""
+            properties:
+              withProtocol:
+                description: a schema with property that ends with protect Protocol name
+                properties:
+                  prop:
+                    type: integer
+                    minimum: 0
+                type: object
+              withMixin:
+                description: a schema with property that ends with protect Mixin name
+                properties:
+                  prop:
+                    type: integer
+                    minimum: 0
+                type: object
+            type: object
+        required:
+        - metadata
+        - spec
+        type: object
+    served: true
+    storage: true

pkg/kube_resource/generator/testdata/suffix_protected_mixin_protocol/models/k8s/apimachinery/pkg/apis/meta/v1/managed_fields_entry.k
@@ -1,0 +1,41 @@
+"""
+This is the managed_fields_entry module in k8s.apimachinery.pkg.apis.meta.v1 package.
+This file was generated by the KCL auto-gen tool. DO NOT EDIT.
+Editing this file might prove futile when you re-run the KCL auto-gen generate command.
+"""
+
+
+schema ManagedFieldsEntry:
+    r"""
+    ManagedFieldsEntry is a workflow-id, a FieldSet and the group version of the resource that the fieldset applies to.
+
+    Attributes
+    ----------
+    apiVersion : str, default is Undefined, optional
+        APIVersion defines the version of this resource that this field set applies to. The format is "group/version" just like the top-level APIVersion field. It is necessary to track the version of a field set because it cannot be automatically converted.
+    fieldsType : str, default is Undefined, optional
+        FieldsType is the discriminator for the different fields format and version. There is currently only one possible value: "FieldsV1"
+    fieldsV1 : any, default is Undefined, optional
+        FieldsV1 holds the first JSON version format as described in the "FieldsV1" type.
+    manager : str, default is Undefined, optional
+        Manager is an identifier of the workflow managing these fields.
+    operation : str, default is Undefined, optional
+        Operation is the type of operation which lead to this ManagedFieldsEntry being created. The only valid values for this field are 'Apply' and 'Update'.
+    time : str, default is Undefined, optional
+        Time is timestamp of when these fields were set. It should always be empty if Operation is 'Apply'
+    """
+
+
+    apiVersion?: str
+
+    fieldsType?: str
+
+    fieldsV1?: any
+
+    manager?: str
+
+    operation?: str
+
+    time?: str
+
+

pkg/kube_resource/generator/testdata/suffix_protected_mixin_protocol/models/k8s/apimachinery/pkg/apis/meta/v1/object_meta.k
@@ -1,0 +1,97 @@
+"""
+This is the object_meta module in k8s.apimachinery.pkg.apis.meta.v1 package.
+This file was generated by the KCL auto-gen tool. DO NOT EDIT.
+Editing this file might prove futile when you re-run the KCL auto-gen generate command.
+"""
+
+
+schema ObjectMeta:
+    r"""
+    ObjectMeta is metadata that all persisted resources must have, which includes all objects users must create.
+
+    Attributes
+    ----------
+    annotations : {str:str}, default is Undefined, optional
+        Annotations is an unstructured key value map stored with a resource that may be set by external tools to store and retrieve arbitrary metadata. They are not queryable and should be preserved when modifying objects. More info: http://kubernetes.io/docs/user-guide/annotations
+    clusterName : str, default is Undefined, optional
+        The name of the cluster which the object belongs to. This is used to distinguish resources with same name and namespace in different clusters. This field is not set anywhere right now and apiserver is going to ignore it if set in create or update request.
+    creationTimestamp : str, default is Undefined, optional
+        CreationTimestamp is a timestamp representing the server time when this object was created. It is not guaranteed to be set in happens-before order across separate operations. Clients may not set this value. It is represented in RFC3339 form and is in UTC.
+
+        Populated by the system. Read-only. Null for lists. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+    deletionGracePeriodSeconds : int, default is Undefined, optional
+        Number of seconds allowed for this object to gracefully terminate before it will be removed from the system. Only set when deletionTimestamp is also set. May only be shortened. Read-only.
+    deletionTimestamp : str, default is Undefined, optional
+        DeletionTimestamp is RFC 3339 date and time at which this resource will be deleted. This field is set by the server when a graceful deletion is requested by the user, and is not directly settable by a client. The resource is expected to be deleted (no longer visible from resource lists, and not reachable by name) after the time in this field, once the finalizers list is empty. As long as the finalizers list contains items, deletion is blocked. Once the deletionTimestamp is set, this value may not be unset or be set further into the future, although it may be shortened or the resource may be deleted prior to this time. For example, a user may request that a pod is deleted in 30 seconds. The Kubelet will react by sending a graceful termination signal to the containers in the pod. After that 30 seconds, the Kubelet will send a hard termination signal (SIGKILL) to the container and after cleanup, remove the pod from the API. In the presence of network partitions, this object may still exist after this timestamp, until an administrator or automated process can determine the resource is fully terminated. If not set, graceful deletion of the object has not been requested.
+
+        Populated by the system when a graceful deletion is requested. Read-only. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+    finalizers : [str], default is Undefined, optional
+        Must be empty before the object is deleted from the registry. Each entry is an identifier for the responsible component that will remove the entry from the list. If the deletionTimestamp of the object is non-nil, entries in this list can only be removed. Finalizers may be processed and removed in any order.  Order is NOT enforced because it introduces significant risk of stuck finalizers. finalizers is a shared field, any actor with permission can reorder it. If the finalizer list is processed in order, then this can lead to a situation in which the component responsible for the first finalizer in the list is waiting for a signal (field value, external system, or other) produced by a component responsible for a finalizer later in the list, resulting in a deadlock. Without enforced ordering finalizers are free to order amongst themselves and are not vulnerable to ordering changes in the list.
+    generateName : str, default is Undefined, optional
+        GenerateName is an optional prefix, used by the server, to generate a unique name ONLY IF the Name field has not been provided. If this field is used, the name returned to the client will be different than the name passed. This value will also be combined with a unique suffix. The provided value has the same validation rules as the Name field, and may be truncated by the length of the suffix required to make the value unique on the server.
+
+        If this field is specified and the generated name exists, the server will NOT return a 409 - instead, it will either return 201 Created or 500 with Reason ServerTimeout indicating a unique name could not be found in the time allotted, and the client should retry (optionally after the time indicated in the Retry-After header).
+
+        Applied only if Name is not specified. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#idempotency
+    generation : int, default is Undefined, optional
+        A sequence number representing a specific generation of the desired state. Populated by the system. Read-only.
+    labels : {str:str}, default is Undefined, optional
+        Map of string keys and values that can be used to organize and categorize (scope and select) objects. May match selectors of replication controllers and services. More info: http://kubernetes.io/docs/user-guide/labels
+    managedFields : [ManagedFieldsEntry], default is Undefined, optional
+        ManagedFields maps workflow-id and version to the set of fields that are managed by that workflow. This is mostly for internal housekeeping, and users typically shouldn't need to set or understand this field. A workflow can be the user's name, a controller's name, or the name of a specific apply path like "ci-cd". The set of fields is always in the version that the workflow used when modifying the object.
+    name : str, default is Undefined, optional
+        Name must be unique within a namespace. Is required when creating resources, although some resources may allow a client to request the generation of an appropriate name automatically. Name is primarily intended for creation idempotence and configuration definition. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names
+    namespace : str, default is Undefined, optional
+        Namespace defines the space within each name must be unique. An empty namespace is equivalent to the "default" namespace, but "default" is the canonical representation. Not all objects are required to be scoped to a namespace - the value of this field for those objects will be empty.
+
+        Must be a DNS_LABEL. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/namespaces
+    ownerReferences : [OwnerReference], default is Undefined, optional
+        List of objects depended by this object. If ALL objects in the list have been deleted, this object will be garbage collected. If this object is managed by a controller, then an entry in this list will point to this controller, with the controller field set to true. There cannot be more than one managing controller.
+    resourceVersion : str, default is Undefined, optional
+        An opaque value that represents the internal version of this object that can be used by clients to determine when objects have changed. May be used for optimistic concurrency, change detection, and the watch operation on a resource or set of resources. Clients must treat these values as opaque and passed unmodified back to the server. They may only be valid for a particular resource or set of resources.
+
+        Populated by the system. Read-only. Value must be treated as opaque by clients and . More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
+    selfLink : str, default is Undefined, optional
+        SelfLink is a URL representing this object. Populated by the system. Read-only.
+
+        DEPRECATED Kubernetes will stop propagating this field in 1.20 release and the field is planned to be removed in 1.21 release.
+    uid : str, default is Undefined, optional
+        UID is the unique in time and space value for this object. It is typically generated by the server on successful creation of a resource and is not allowed to change on PUT operations.
+
+        Populated by the system. Read-only. More info: http://kubernetes.io/docs/user-guide/identifiers#uids
+    """
+
+
+    annotations?: {str:str}
+
+    clusterName?: str
+
+    creationTimestamp?: str
+
+    deletionGracePeriodSeconds?: int
+
+    deletionTimestamp?: str
+
+    finalizers?: [str]
+
+    generateName?: str
+
+    generation?: int
+
+    labels?: {str:str}
+
+    managedFields?: [ManagedFieldsEntry]
+
+    name?: str
+
+    namespace?: str
+
+    ownerReferences?: [OwnerReference]
+
+    resourceVersion?: str
+
+    selfLink?: str
+
+    uid?: str
+
+

pkg/kube_resource/generator/testdata/suffix_protected_mixin_protocol/models/k8s/apimachinery/pkg/apis/meta/v1/owner_reference.k
@@ -1,0 +1,41 @@
+"""
+This is the owner_reference module in k8s.apimachinery.pkg.apis.meta.v1 package.
+This file was generated by the KCL auto-gen tool. DO NOT EDIT.
+Editing this file might prove futile when you re-run the KCL auto-gen generate command.
+"""
+
+
+schema OwnerReference:
+    r"""
+    OwnerReference contains enough information to let you identify an owning object. An owning object must be in the same namespace as the dependent, or be cluster-scoped, so there is no namespace field.
+
+    Attributes
+    ----------
+    apiVersion : str, default is Undefined, required
+        API version of the referent.
+    blockOwnerDeletion : bool, default is Undefined, optional
+        If true, AND if the owner has the "foregroundDeletion" finalizer, then the owner cannot be deleted from the key-value store until this reference is removed. Defaults to false. To set this field, a user needs "delete" permission of the owner, otherwise 422 (Unprocessable Entity) will be returned.
+    controller : bool, default is Undefined, optional
+        If true, this reference points to the managing controller.
+    kind : str, default is Undefined, required
+        Kind of the referent. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+    name : str, default is Undefined, required
+        Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names
+    uid : str, default is Undefined, required
+        UID of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#uids
+    """
+
+
+    apiVersion: str
+
+    blockOwnerDeletion?: bool
+
+    controller?: bool
+
+    kind: str
+
+    name: str
+
+    uid: str
+
+

pkg/kube_resource/generator/testdata/suffix_protected_mixin_protocol/models/kcl_lang_io_v1alpha1_builtin_names.k
@@ -1,0 +1,87 @@
+"""
+This file was generated by the KCL auto-gen tool. DO NOT EDIT.
+Editing this file might prove futile when you re-run the KCL auto-gen generate command.
+"""
+import k8s.apimachinery.pkg.apis.meta.v1
+
+
+schema BuiltinNames:
+    r"""
+    kcl lang io v1alpha1 builtin names
+
+    Attributes
+    ----------
+    apiVersion : str, default is "kcl-lang.io/v1alpha1", required
+        APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+    kind : str, default is "BuiltinNames", required
+        Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+    metadata : v1.ObjectMeta, default is Undefined, required
+        metadata
+    spec : KclLangIoV1alpha1BuiltinNamesSpec, default is Undefined, required
+        spec
+    """
+
+
+    apiVersion: "kcl-lang.io/v1alpha1" = "kcl-lang.io/v1alpha1"
+
+    kind: "BuiltinNames" = "BuiltinNames"
+
+    metadata: v1.ObjectMeta
+
+    spec: KclLangIoV1alpha1BuiltinNamesSpec
+
+
+schema KclLangIoV1alpha1BuiltinNamesSpec:
+    r"""
+    kcl lang io v1alpha1 builtin names spec
+
+    Attributes
+    ----------
+    withMixin : KclLangIoV1alpha1BuiltinNamesSpecWithMixinSchema, default is Undefined, optional
+        with mixin
+    withProtocol : KclLangIoV1alpha1BuiltinNamesSpecWithProtocolSchema, default is Undefined, optional
+        with protocol
+    """
+
+
+    withMixin?: KclLangIoV1alpha1BuiltinNamesSpecWithMixinSchema
+
+    withProtocol?: KclLangIoV1alpha1BuiltinNamesSpecWithProtocolSchema
+
+
+schema KclLangIoV1alpha1BuiltinNamesSpecWithMixinSchema:
+    r"""
+    a schema with property that ends with protect Mixin name
+
+    Attributes
+    ----------
+    prop : int, default is Undefined, optional
+        prop
+    """
+
+
+    prop?: int
+
+
+    check:
+        prop >= 0 if prop not in [None, Undefined]
+
+
+schema KclLangIoV1alpha1BuiltinNamesSpecWithProtocolSchema:
+    r"""
+    a schema with property that ends with protect Protocol name
+
+    Attributes
+    ----------
+    prop : int, default is Undefined, optional
+        prop
+    """
+
+
+    prop?: int
+
+
+    check:
+        prop >= 0 if prop not in [None, Undefined]
+
+